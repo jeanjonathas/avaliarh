3726c0330effcb5c6b403dc8d50177d2fdb4e9bb
jeanjonathas <jeanjonathasfb@gmail.com>
2025-03-06 07:36:31 -0300

    Corrigir caminhos de importação do arquivo de autenticação em todos os endpoints da API

--- pages/api/admin/questions/index.ts
+++ pages/api/admin/questions/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '../../auth/[...nextauth]'
+import { authOptions } from '../../../auth/[...nextauth]'
 
 const prisma = new PrismaClient()
 
